3d8760e0015f907f8c3b839df6bc1a16030634ca
ekaterina <mozhegova04@gmail.com>
2024-07-08 18:58:06 +0300

    Added log messages

--- build/overlord100_controller/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/ws/src/Overlord100-controller/install/overlord100_msgs/share/overlord100_msgs/package.sh"
--- build/overlord100_msgs/overlord100_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ws/src/Overlord100-controller/build/overlord100_msgs/overlord100_msgs__py
-# Build directory: /home/ws/src/Overlord100-controller/build/overlord100_msgs/overlord100_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
