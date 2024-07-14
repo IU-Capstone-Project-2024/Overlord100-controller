94333444ef02c4dc40db427208da9879884aed01
ekaterina <mozhegova04@gmail.com>
2024-07-14 19:54:42 +0300

    Fixed the bug

--- overlord100_controller/src/controller.cpp
+++ overlord100_controller/src/controller.cpp
@@ -1,17 +1,18 @@
-#include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <rclcpp/rclcpp.hpp>
+
 #include "overlord100_msgs/msg/wheels_data.hpp"
 
 #define M_PI 3.14159265358979323846 /* pi */
 
-class DiffDriveController : public rclcpp::Node
-{
-public:
-    DiffDriveController() : Node("diff_drive_controller")
-    {
+class DiffDriveController : public rclcpp::Node {
+   public:
+    DiffDriveController() : Node("diff_drive_controller") {
         // Parameters for the differential drive robot
-        this->declare_parameter<double>("robot_base_", 0.575);    // Distance between the wheels
-        this->declare_parameter<double>("wheel_radius_", 0.0655); // Radius of the wheels
+        this->declare_parameter<double>("robot_base_",
+                                        0.575);  // Distance between the wheels
+        this->declare_parameter<double>("wheel_radius_",
+                                        0.0655);  // Radius of the wheels
         this->declare_parameter<std::string>("measurement_units_", "rpm");
 
         this->get_parameter("robot_base_", robot_base_);
@@ -20,24 +21,32 @@ public:
 
         // Subscriber to the cmd_vel topic
         cmd_vel_sub_ = this->create_subscription<geometry_msgs::msg::Twist>(
-            "cmd_vel", 10, std::bind(&DiffDriveController::cmdVelCallback, this, std::placeholders::_1));
+            "cmd_vel", 10,
+            std::bind(&DiffDriveController::cmdVelCallback, this,
+                      std::placeholders::_1));
 
         // Publisher for the wheel velocities
-        wheels_pub_ = this->create_publisher<overlord100_msgs::msg::WheelsData>("wheels_control", 10);
+        wheels_pub_ = this->create_publisher<overlord100_msgs::msg::WheelsData>(
+            "wheels_control", 10);
     }
 
-private:
-    void cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg)
-    {
+   private:
+    void cmdVelCallback(const geometry_msgs::msg::Twist::SharedPtr msg) {
         // Extract linear and angular velocities from the message
         double angular_velocity = msg->angular.z;
-        double lin_vx = msg->linear.x;
-        double lin_vy = msg->linear.y;
-        double linear_velocity = std::sqrt(lin_vx * lin_vx + lin_vy * lin_vy);
+        // double lin_vx = msg->linear.x;
+        // // double lin_vy = msg->linear.y;
+        // double linear_velocity = lin_vx;
+        // // = std::sqrt(lin_vx * lin_vx + lin_vy * lin_vy);
+        double linear_velocity = msg->linear.x;
 
         // Compute the wheel velocities
-        double angular_right = (2 * linear_velocity + this->half_robot_base_ * angular_velocity) / (2 * wheel_radius_);
-        double angular_left = (2 * linear_velocity - this->half_robot_base_ * angular_velocity) / (2 * wheel_radius_);
+        double angular_right =
+            (linear_velocity + this->half_robot_base_ * angular_velocity) /
+            (wheel_radius_);
+        double angular_left =
+            (linear_velocity - this->half_robot_base_ * angular_velocity) /
+            (wheel_radius_);
         double angular_right_rpm = angular_right * 30 / M_PI;
         double angular_left_rpm = angular_left * 30 / M_PI;
 
@@ -46,9 +55,12 @@ private:
         wheels_msg.left = static_cast<float>(angular_left_rpm);
         wheels_msg.right = static_cast<float>(angular_right_rpm);
 
-        RCLCPP_INFO(this->get_logger(), "Publishing wheels_msg.left: '%s'", std::to_string(wheels_msg.left).c_str());
-        RCLCPP_INFO(this->get_logger(), "Publishing wheels_msg.right: '%s'", std::to_string(wheels_msg.right).c_str());
-        RCLCPP_INFO(this->get_logger(), "Robot base '%d'", this->get_parameter("robot_base_", robot_base_));
+        RCLCPP_INFO(this->get_logger(), "Publishing wheels_msg.left: '%s'",
+                    std::to_string(wheels_msg.left).c_str());
+        RCLCPP_INFO(this->get_logger(), "Publishing wheels_msg.right: '%s'",
+                    std::to_string(wheels_msg.right).c_str());
+        RCLCPP_INFO(this->get_logger(), "Robot base '%d'",
+                    this->get_parameter("robot_base_", robot_base_));
         std::cout << robot_base_ << std::endl;
 
         wheels_pub_->publish(wheels_msg);
@@ -62,10 +74,84 @@ private:
     double half_robot_base_;
 };
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<DiffDriveController>());
     rclcpp::shutdown();
     return 0;
 }
+
+// #include <geometry_msgs/msg/twist.hpp>
+// #include <rclcpp/rclcpp.hpp>
+
+// #include "overlord100_msgs/msg/wheels_data.hpp"
+
+// #define M_PI 3.14159265358979323846 /* pi */
+// using namespace std::chrono_literals;
+
+// class DiffDriveController : public rclcpp::Node {
+//    public:
+//     DiffDriveController() : Node("diff_drive_controller") {
+//         // Parameters for the differential drive robot
+//         this->declare_parameter<double>("robot_base_",
+//                                         0.575);  // Distance between the
+//                                         wheels
+//         this->declare_parameter<double>("wheel_radius_",
+//                                         0.0655);  // Radius of the wheels
+//         this->declare_parameter<std::string>("measurement_units_", "rpm");
+
+//         this->get_parameter("robot_base_", robot_base_);
+//         this->get_parameter("wheel_radius_", wheel_radius_);
+//         this->half_robot_base_ = robot_base_ / 2;
+
+//         // // Subscriber to the cmd_vel topic
+//         // cmd_vel_sub_ =
+//         this->create_subscription<geometry_msgs::msg::Twist>(
+//         //     "cmd_vel", 10,
+//         //     std::bind(&DiffDriveController::cmdVelCallback, this,
+//         //               std::placeholders::_1));
+
+//         // Publisher for the wheel velocities
+//         wheels_pub_ =
+//         this->create_publisher<overlord100_msgs::msg::WheelsData>(
+//             "wheels_control", 10);
+
+//         timer_ = this->create_wall_timer(500ms,
+//         std::bind(&DiffDriveController::timer_callback, this));
+//     }
+
+//    private:
+//     void create_wall_timer(const geometry_msgs::msg::Twist::SharedPtr msg) {
+//         double angular_right_rpm = 100 * 30 / M_PI;
+//         double angular_left_rpm = 100 * 30 / M_PI;
+
+//         // Create and publish the custom message
+//         auto wheels_msg = overlord100_msgs::msg::WheelsData();
+//         wheels_msg.left = static_cast<float>(angular_left_rpm);
+//         wheels_msg.right = static_cast<float>(angular_right_rpm);
+
+//         RCLCPP_INFO(this->get_logger(), "Publishing wheels_msg.left: '%s'",
+//                     std::to_string(wheels_msg.left).c_str());
+//         RCLCPP_INFO(this->get_logger(), "Publishing wheels_msg.right: '%s'",
+//                     std::to_string(wheels_msg.right).c_str());
+//         RCLCPP_INFO(this->get_logger(), "Robot base '%d'",
+//                     this->get_parameter("robot_base_", robot_base_));
+//         std::cout << robot_base_ << std::endl;
+
+//         wheels_pub_->publish(wheels_msg);
+//     }
+
+//     // rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr
+//     cmd_vel_sub_;
+//     rclcpp::Publisher<overlord100_msgs::msg::WheelsData>::SharedPtr
+//     wheels_pub_; rclcpp::TimerBase::SharedPtr timer_; double wheel_radius_;
+//     double robot_base_;
+//     double half_robot_base_;
+// };
+
+// int main(int argc, char *argv[]) {
+//     rclcpp::init(argc, argv);
+//     rclcpp::spin(std::make_shared<DiffDriveController>());
+//     rclcpp::shutdown();
+//     return 0;
+// }
